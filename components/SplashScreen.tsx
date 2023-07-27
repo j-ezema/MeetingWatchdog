8db4f1dd57a40fdb0255af80754bf07188ba9ebd
Luke Richards <luke.richards@carbonedge.com>
2023-07-27 11:00:49 -0400

    splash screen changes
    android version blank (should load quickly), react version fixed

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Image } from 'react-native-elements';
+import { Image} from 'react-native-elements';
 import {
     View,
     Text,
@@ -13,13 +13,14 @@ import {
 
 
 } from 'react-native';
+import { colors } from '../assets/Styles';
 
 
 
 export const SplashScreen = () => {
     return(
-        <View style={{flex:1}}>
-            <Image source={require('../assets/images/splash.png')} style={{flex:1}} />
+        <View style={{flex:1, backgroundColor:colors.oxfordBlue,flexDirection:'row'}}>
+            <Image source={require('../assets/images/splash.png')} style={{flex:1,maxWidth:'100%', maxHeight:'100%', aspectRatio: 9/16, resizeMode:'contain'}} />
         </View>
     )
 }
